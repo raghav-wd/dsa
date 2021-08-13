ebb5b624ba69651ebbb130db156baeadf303e17b
Devansh Gupta <41248639+raghav-wd@users.noreply.github.com>
2021-08-13 23:51:42 +0530

    Update Eleminate repeated elements in 2 array one by one.java

--- Problems/Eleminate repeated elements in 2 array one by one.java
+++ Problems/Eleminate repeated elements in 2 array one by one.java
@@ -1,1 +1,27 @@
-
+    void main(){
+        int arr1[] = {1,2,4,5,3};
+        int arr2[] = {2,6,8,10,3};
+        int n1 = 5, n2 = 4;
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for(int i : arr1){
+            if(map.containsKey(i))
+                map.put(i, map.get(i)+1);
+            else map.put(i, 1);
+        }
+        
+        for(int i : arr2){
+            if(map.containsKey(i))
+                map.put(i, map.get(i)+1);
+            else map.put(i, 1);
+        }
+        
+        for(int i: arr1)
+            if(map.get(i) == 1)
+                System.out.print(i+" ");
+        
+        for(int i: arr2)
+            if(map.get(i) == 1)
+                System.out.print(i+ " ");
+}
