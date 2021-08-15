6a4848dbe8d3bd5c0ae228b6c9fe3089a12c37b1
Devansh Gupta <41248639+raghav-wd@users.noreply.github.com>
2021-08-15 23:48:32 +0530

    Add mapxset

--- CheatSheets/Java.java
+++ CheatSheets/Java.java
@@ -5,3 +5,6 @@ map.get("A"); //Returns 1
 map.containsKey("A"); //Returns true
 map.putAll(map1); //copies data from map1 into map - map and map1 should hava same data variables declaration
 map.clear(); //emties the map
+
+map.entrySet(); // returns map as a set/array
+map.values(); // returns collection view of values in map
