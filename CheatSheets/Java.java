9d5b5b7ee729cd7ef36faa51fd2d602e6ac4a6ff
Devansh Gupta <41248639+raghav-wd@users.noreply.github.com>
2021-08-15 21:11:28 +0530

    HashMap cheats added

---
+++ CheatSheets/Java.java
@@ -0,0 +1,7 @@
+//HashMap
+HashMap<String, Character, Integer> map = new HashMap<>();
+map.put("A", 1);
+map.get("A"); //Returns 1
+map.containsKey("A"); //Returns true
+map.putAll(map1); //copies data from map1 into map - map and map1 should hava same data variables declaration
+map.clear(); //emties the map
