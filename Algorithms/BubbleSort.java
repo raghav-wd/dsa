f2c0cad5cfb33515aab6012c2a75400f3953ae90
Devansh Gupta <41248639+raghav-wd@users.noreply.github.com>
2021-08-16 11:06:45 +0530

    Another way of bubble sort loop

--- Algorithms/BubbleSort.java
+++ Algorithms/BubbleSort.java
@@ -13,3 +13,14 @@ void bubbleSort() {
   for(int item : arr)
   System.out.println(item);
 }
+
+//Or
+for (int i = 0; i < n-1; i++)
+   for (int j = 0; j < n-i-1; j++)
+       if (arr[j] < arr[j+1])
+       {
+         // swap arr[j+1] and arr[j]
+          int temp = arr[j];
+          arr[j] = arr[j+1];
+          arr[j+1] = temp;
+        }
