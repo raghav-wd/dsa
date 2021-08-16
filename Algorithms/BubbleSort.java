b81d5f690c4ca75e8ed2f94643a18ac62aadd40b
Devansh Gupta <41248639+raghav-wd@users.noreply.github.com>
2021-08-16 11:16:15 +0530

    Optimised if array already sorted best case O(n)

--- Algorithms/BubbleSort.java
+++ Algorithms/BubbleSort.java
@@ -24,3 +24,32 @@ for (int i = 0; i < n-1; i++)
           arr[j] = arr[j+1];
           arr[j+1] = temp;
         }
+
+//Optimised
+//Worst Case O(n^2)
+//Best Case O(n) if array already sorted
+static void bubbleSort(int arr[], int n)
+    {
+        int i, j, temp;
+        boolean swapped;
+        for (i = 0; i < n - 1; i++) 
+        {
+            swapped = false;
+            for (j = 0; j < n - i - 1; j++) 
+            {
+                if (arr[j] > arr[j + 1]) 
+                {
+                    // swap arr[j] and arr[j+1]
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+  
+            // IF no two elements were 
+            // swapped by inner loop, then break
+            if (swapped == false)
+                break;
+        }
+    }
