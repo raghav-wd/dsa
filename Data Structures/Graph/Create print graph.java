62f9b9076a35f9a013eaba7181689e4b7977e33f
Devansh Gupta <41248639+raghav-wd@users.noreply.github.com>
2021-08-18 16:36:45 +0530

    Create Create print graph.java

---
+++ Data Structures/Graph/Create print graph.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Graph {
+    static void addEdge(ArrayList<ArrayList<Integer>> adj, int u, int v){
+        adj.get(u).add(v);
+        adj.get(v).add(u);
+    }
+    
+    static void printGraph(ArrayList<ArrayList<Integer>> adj){
+        for(int i = 0; i<adj.size(); i++){
+            System.out.println("\nAdjacency list of vertex" + i);
+            for(int j = 0; j<adj.get(i).size(); j++)
+                System.out.print("->"+adj.get(i).get(j));
+            System.out.println();
+        }
+    }
+    
+    public static void main(String args[]){
+        int V = 2;
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>(V);
+        
+        for(int i = 0; i<V; i++)
+            adj.add(new ArrayList<Integer>());
+        
+        addEdge(adj, 0, 1);
+
+        printGraph(adj);
+    }
+}
