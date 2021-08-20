5257c0f7aa5fdc57000c1caf28ad0fd1e6ee23b4
Devansh Gupta <41248639+raghav-wd@users.noreply.github.com>
2021-08-20 20:12:06 +0530

    Dfs post order add

---
+++ Data Structures/Binary Tree/DFS (inorder, preorder, postorder).java
@@ -0,0 +1,35 @@
+import java.util.*;
+class Node {
+    int val;
+    Node left = null, right = null;
+
+    Node(int val) {
+        this.val = val;
+    }
+  
+    void postOrder(Node head) {
+        if (head == null) {
+            return;
+        }
+        postOrder(head.left);
+        postOrder(head.right);
+        System.out.println(head.val);
+    }
+} 
+
+class RightVibe {
+    public static void main(String args[]) {
+        System.out.println("=========================================================\n");
+
+        Node root = new Node(5);
+        root.left = new Node(4);
+        root.right = new Node(3);
+        root.left.left = new Node(2);
+        root.left.right = new Node(1);
+
+        root.bfs(root);
+        root.postOrder(root);
+
+        System.out.println("\n");
+    }
+}
