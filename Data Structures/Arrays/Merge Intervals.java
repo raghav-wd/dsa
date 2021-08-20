757373c8e1e03ae8f6ce714aa14daa53df57b08b
Devansh Gupta <41248639+raghav-wd@users.noreply.github.com>
2021-08-20 16:43:03 +0530

    Merge Intervals in 2D array

---
+++ Data Structures/Arrays/Merge Intervals.java
@@ -0,0 +1,38 @@
+// Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+
+ 
+
+// Example 1:
+
+// Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+// Output: [[1,6],[8,10],[15,18]]
+// Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+// Example 2:
+
+// Input: intervals = [[1,4],[4,5]]
+// Output: [[1,5]]
+// Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+ 
+
+// Constraints:
+
+// 1 <= intervals.length <= 104
+// intervals[i].length == 2
+// 0 <= starti <= endi <= 104
+
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        LinkedList<int[]> merged = new LinkedList<>();
+        Arrays.sort(intervals, Comparator.comparingDouble(o -> o[0]));
+        
+        for(int[] interval : intervals){
+            if(merged.isEmpty() || merged.getLast()[1] < interval[0] ){
+                merged.add(interval);
+            }
+            else {
+                merged.getLast()[1] = Math.max(merged.getLast()[1], interval[1]);
+            }
+        }
+        return merged.toArray(new int[merged.size()][]);
+    }
+}
