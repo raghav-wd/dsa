e56df8cb55e14d270cc2a1ba195817c47677b574
Devansh Gupta <41248639+raghav-wd@users.noreply.github.com>
2021-09-09 13:52:03 +0530

    rank() and dense_rank() add

--- Dbms/Basic.sql
+++ Dbms/Basic.sql
@@ -1,6 +1,10 @@
-//Creating and adding into the table
+-- //Creating and adding into the table
 CREATE TABLE IF NOT EXISTS People(id int PRIMARY KEY AUTO_INCREMENT, Name varchar(255), Rating int);
 TRUNCATE TABLE People;
 INSERT INTO People(Name, Rating) VALUES('Raghav', 6);
-INSERT INTO People(Name, Rating) VALUES('Isabel', 10);
+INSERT INTO People(Name, Rating) VALUES('Lebasi', 10);
 SELECT * FROM People
+
+-- //Rank ( Rank= 1, 1, 3, 4, 4, 6 and DENSE_RANK= 1, 1, 2, 3, 3, 4)
+SELECT Score, Rank() OVER (ORDER BY Score DESC) AS 'Rank' FROM Scores
+SELECT Score, DENSE_Rank() OVER (ORDER BY Score DESC) AS 'Rank' FROM Scores 
