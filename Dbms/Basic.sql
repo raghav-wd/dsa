783fa629deaa3a73ff042daafe8eb3c6691bd4bf
Devansh Gupta <41248639+raghav-wd@users.noreply.github.com>
2021-09-10 23:37:30 +0530

    Update add

--- Dbms/Basic.sql
+++ Dbms/Basic.sql
@@ -8,3 +8,8 @@ SELECT * FROM People
 -- //Rank ( Rank= 1, 1, 3, 4, 4, 6 and DENSE_RANK= 1, 1, 2, 3, 3, 4)
 SELECT Score, Rank() OVER (ORDER BY Score DESC) AS 'Rank' FROM Scores
 SELECT Score, DENSE_Rank() OVER (ORDER BY Score DESC) AS 'Rank' FROM Scores 
+
+-- Update
+UPDATE People
+SET Name = 'Devansh'
+WHERE Name = 'Raghav'
