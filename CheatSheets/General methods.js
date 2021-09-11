5c3cbf1abc053f55cb0200b65adaf5ca35802ba4
Devansh Gupta <41248639+raghav-wd@users.noreply.github.com>
2021-09-11 18:36:42 +0530

    Array function add

---
+++ CheatSheets/General methods.js
@@ -0,0 +1,4 @@
+// Array Functions - transform the array according to the applied function and return the updated array
+map() // It applies a given function on all the elements of the array and returns the updated array
+filter() // It filters the elements of the array that return false for the applied condition
+reduce() // It reduces all the elements of the array to a single value by repeatedly applying a function
