e05d2b24159a678e9b788b015bdf719c5b5a15cf
Devansh Gupta <41248639+raghav-wd@users.noreply.github.com>
2021-09-11 20:12:48 +0530

    Update General methods.js

--- CheatSheets/General methods.js
+++ CheatSheets/General methods.js
@@ -1,4 +1,5 @@
 // Array Functions - transform the array according to the applied function and return the updated array
-map() // It applies a given function on all the elements of the array and returns the updated array
-filter() // It filters the elements of the array that return false for the applied condition
-reduce() // It reduces all the elements of the array to a single value by repeatedly applying a function
+ls = [1, 2, 3, 4, 5]
+ls.map(item => item + 1) // [2, 3, 4, 5, 6] // It applies a given function on all the elements of the array and returns the updated array
+ls.filter(item => item & 2 == 0) // [2, 4] // It filters the elements of the array that return false for the applied condition
+ls.reduce( (accumulator, item) => accumulator + item, 100 ) // 115 // It reduces all the elements of the array to a single value by repeatedly applying a function
