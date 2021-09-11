e9a1e2e6a5f3180fef908b12ad63879b3f3c0161
Devansh Gupta <41248639+raghav-wd@users.noreply.github.com>
2021-09-11 18:58:26 +0530

    some terms add

---
+++ CheatSheets/Teminology.js
@@ -0,0 +1,5 @@
+() // after a variable means we are calling a function with that name
+=> // fat arrow
+`` // template literal
+${} // argument placeholder, what we place in between would be rendered dynamically at the runtime
+... // spread operator
