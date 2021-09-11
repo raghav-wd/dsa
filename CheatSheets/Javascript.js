70162422e1aa58d33a2dc541d9c93fc565b0f89b
Devansh Gupta <41248639+raghav-wd@users.noreply.github.com>
2021-09-11 20:12:59 +0530

    Update Javascript.js

--- CheatSheets/Javascript.js
+++ CheatSheets/Javascript.js
@@ -2,10 +2,6 @@ var // has global scope
 let // has block scope
 const // has block scope and is a read-only/constant value
 
-// Notes
-() // after a variable means we are calling a function with that name
-=> // fat arrow
-
 const person = {
   name: "Raghav",
   age: 21,
@@ -17,3 +13,4 @@ person.talk()
 person['dataMember']() // dataMember = 'talk' // dynamically calling a function
 console.log(person.name)
 console.log(person[dataMember]) // dataMember = 'name' // dynamically access object member
+
