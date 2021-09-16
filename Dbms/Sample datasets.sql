00553ab7db108feac2b5a32622faf57fbe4c1bb1
Devansh Gupta <41248639+raghav-wd@users.noreply.github.com>
2021-09-16 18:21:28 +0530

    Department table add

--- Dbms/Sample datasets.sql
+++ Dbms/Sample datasets.sql
@@ -1,5 +1,3 @@
--- Employees Table
-CREATE TABLE If not exists employees(empno int AUTO_INCREMENT PRIMARY key, employeename varchar(255), salary int, deptno int);
 insert into employees(employeename, salary, deptno) value('KAYLING', 6000, 1001);
 insert into employees(employeename, salary, deptno) value('BLAZE', 2750, 3001);
 insert into employees(employeename, salary, deptno) value('CLARE', 2550, 1001);
@@ -14,3 +12,10 @@ insert into employees(employeename, salary, deptno) value('TUCKER', 1600, 3001);
 insert into employees(employeename, salary, deptno) value('ADNRES', 1200, 2001);
 insert into employees(employeename, salary, deptno) value('JULIUS', 1050, 3001);
 insert into employees(employeename, salary, deptno) value('MARKER', 1400, 1001);
+
+CREATE TABLE if not exists department (deptno int, deptname varchar(255));
+insert into department (deptno, deptname) VALUES(1001, 'Software Engineer');
+insert into department (deptno, deptname) VALUES(2001, 'R&D Engineer');
+insert into department (deptno, deptname) VALUES(3001, 'ML Engineer');
+
+-- SELECT deptname from department where deptno in (SELECT deptno from employees group by deptno having count(*) > 3);
