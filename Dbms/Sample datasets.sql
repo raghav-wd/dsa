d3b707bfb79344a37c32f17e21d2491353fcb454
Devansh Gupta <41248639+raghav-wd@users.noreply.github.com>
2021-09-16 17:30:03 +0530

    Employees table

---
+++ Dbms/Sample datasets.sql
@@ -0,0 +1,16 @@
+-- Employees Table
+CREATE TABLE If not exists employees(empno int AUTO_INCREMENT PRIMARY key, employeename varchar(255), salary int, deptno int);
+insert into employees(employeename, salary, deptno) value('KAYLING', 6000, 1001);
+insert into employees(employeename, salary, deptno) value('BLAZE', 2750, 3001);
+insert into employees(employeename, salary, deptno) value('CLARE', 2550, 1001);
+insert into employees(employeename, salary, deptno) value('JONAS', 2957, 2001);
+insert into employees(employeename, salary, deptno) value('SCARLET', 3100, 2001);
+insert into employees(employeename, salary, deptno) value('FRANK', 3100, 2001);
+insert into employees(employeename, salary, deptno) value('SANDRINE', 900, 2001);
+insert into employees(employeename, salary, deptno) value('ADELYN', 1700, 3001);
+insert into employees(employeename, salary, deptno) value('WADE', 1350, 3001);
+insert into employees(employeename, salary, deptno) value('MADDEN', 1350, 3001);
+insert into employees(employeename, salary, deptno) value('TUCKER', 1600, 3001);
+insert into employees(employeename, salary, deptno) value('ADNRES', 1200, 2001);
+insert into employees(employeename, salary, deptno) value('JULIUS', 1050, 3001);
+insert into employees(employeename, salary, deptno) value('MARKER', 1400, 1001);
