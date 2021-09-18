471a6be52581860739491cb16bbb2d1867ec9cd7
Devansh Gupta <41248639+raghav-wd@users.noreply.github.com>
2021-09-18 23:25:48 +0530

    unintended side effects

--- CheatSheets/Teminology.js
+++ CheatSheets/Teminology.js
@@ -3,3 +3,11 @@
 `` // template literal
 ${} // argument placeholder, what we place in between would be rendered dynamically at the runtime
 ... // spread operator
+
+
+// Unintended side-effects // does bunch of stuff instead of just saving the user hence those our side-effects
+function saveUser(user){ 
+  // Validation for our user
+  // Printing out all our validation error
+  // Saving the user
+}
